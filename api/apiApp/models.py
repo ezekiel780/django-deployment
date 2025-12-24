59a0d63aaf84c113d2098139f092d2563dcc08f1
Ezekiel <ezekielbalogun56@gmail.com>
2025-12-23 20:22:39 -0800

    Add apiApp with models, serializers, and views

---
+++ api/apiApp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture = models.URLField(blank=True, null=True)
+    def __str__(self):
+        return self.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category_img', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='product_img', blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
