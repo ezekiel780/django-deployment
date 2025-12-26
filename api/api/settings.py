df3155079b3f8ad33dacf6476291671e6d3e483c
Ezekiel <ezekielbalogun56@gmail.com>
2025-12-25 18:54:56 -0800

    Fix product_list view and serializers, Dockerfile update

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+
 ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get('ALLOWED_HOSTS', '').split(',')
-    )
+    host.strip()
+    for host in os.environ.get("ALLOWED_HOSTS", "").split(",")
+    if host.strip()
 )
 
 
--- api/apiApp/views.py
+++ api/apiApp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import CategoryDetailSerializer, CategorySerializer, ProductDetailSerializer, ProductSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.filter(featured=True)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    serializer = ProductDetailSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializers = CategorySerializer(categories, many=True)
+    return Response(serializers.data)
+
+@api_view(['GET'])
+def category_detail(request, slug):
+    category = Category.objects.get(slug=slug)
+    serializer = CategoryDetailSerializer(category)
+    return Response(serializer.data)
+
